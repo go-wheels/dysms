01ae3c9ef9eefb79fbd3c8d4cb7a41918f2a5d27
flin7 <flin7@outlook.com>
2022-01-25 12:15:47 +0800

    first commit

---
+++ client.go
@@ -0,0 +1,77 @@
+package dysms
+
+import (
+	"encoding/json"
+
+	openapi "github.com/alibabacloud-go/darabonba-openapi/client"
+	dysmsapi "github.com/alibabacloud-go/dysmsapi-20170525/v2/client"
+	"github.com/alibabacloud-go/tea/tea"
+)
+
+const endpoint = "dysmsapi.aliyuncs.com"
+
+type TemplateParam map[string]string
+
+type Client struct {
+	dysmsapiClient *dysmsapi.Client
+}
+
+func NewClient(accessKeyID, accessKeySecret string) (client *Client, err error) {
+	config := &openapi.Config{
+		AccessKeyId:     tea.String(accessKeyID),
+		AccessKeySecret: tea.String(accessKeySecret),
+		Endpoint:        tea.String(endpoint),
+	}
+	client = new(Client)
+	client.dysmsapiClient, err = dysmsapi.NewClient(config)
+	return
+}
+
+// 发送短信
+func (c Client) SendSMS(phoneNumbers, signName, templateCode string, templateParam interface{}) (resp *dysmsapi.SendSmsResponse, err error) {
+	req := &dysmsapi.SendSmsRequest{
+		PhoneNumbers: tea.String(phoneNumbers),
+		SignName:     tea.String(signName),
+		TemplateCode: tea.String(templateCode),
+	}
+	if templateParam != nil {
+		var data []byte
+		data, err = json.Marshal(templateParam)
+		if err != nil {
+			return
+		}
+		req.TemplateParam = tea.String(string(data))
+	}
+	resp, err = c.dysmsapiClient.SendSms(req)
+	if err != nil {
+		return
+	}
+	if tea.StringValue(resp.Body.Code) != "OK" {
+		err = newSDKError(resp.Body.Code, resp.Body.Message)
+	}
+	return
+}
+
+// 创建短链
+func (c Client) AddShortURL(sourceURL, shortURLName, effectiveDays string) (resp *dysmsapi.AddShortUrlResponse, err error) {
+	req := &dysmsapi.AddShortUrlRequest{
+		SourceUrl:     tea.String(sourceURL),
+		ShortUrlName:  tea.String(shortURLName),
+		EffectiveDays: tea.String(effectiveDays),
+	}
+	resp, err = c.dysmsapiClient.AddShortUrl(req)
+	if err != nil {
+		return
+	}
+	if tea.StringValue(resp.Body.Code) != "OK" {
+		err = newSDKError(resp.Body.Code, resp.Body.Message)
+	}
+	return
+}
+
+func newSDKError(code, message *string) *tea.SDKError {
+	return tea.NewSDKError(map[string]interface{}{
+		"code":    tea.StringValue(code),
+		"message": tea.StringValue(message),
+	})
+}
---
+++ go.mod
@@ -0,0 +1,24 @@
+module github.com/go-wheels/dysms
+
+go 1.17
+
+require (
+	github.com/alibabacloud-go/darabonba-openapi v0.1.14
+	github.com/alibabacloud-go/dysmsapi-20170525/v2 v2.0.9
+	github.com/alibabacloud-go/tea v1.1.17
+)
+
+require (
+	github.com/alibabacloud-go/alibabacloud-gateway-spi v0.0.2 // indirect
+	github.com/alibabacloud-go/debug v0.0.0-20190504072949-9472017b5c68 // indirect
+	github.com/alibabacloud-go/endpoint-util v1.1.0 // indirect
+	github.com/alibabacloud-go/openapi-util v0.0.10 // indirect
+	github.com/alibabacloud-go/tea-utils v1.4.3 // indirect
+	github.com/aliyun/credentials-go v1.1.3 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/tjfoc/gmsm v1.4.1 // indirect
+	golang.org/x/net v0.0.0-20220121210141-e204ce36a2ba // indirect
+	gopkg.in/ini.v1 v1.66.3 // indirect
+)
